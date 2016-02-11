0ea01654bf0961fe305abdf8346509e5ebc089f2
Andrei Lescu <andrei.lescu@dialogdata.de>
2016-02-11 18:37:10 +0200

    Clean Up

--- hash.code/src/main/java/com/binary/thinkers/hash/code/App.java
+++ hash.code/src/main/java/com/binary/thinkers/hash/code/App.java
@@ -1,14 +1,10 @@
 package com.binary.thinkers.hash.code;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hey");
+        
     }
 }
